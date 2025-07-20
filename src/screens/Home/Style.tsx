711bee536d4872ab0866620728215d6940ff9dd6
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-07-20 19:09:48 -0300

    feat: create logo component

--- src/screens/Home/Style.tsx
+++ src/screens/Home/Style.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native"
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#4533aa',
+    backgroundColor: '#5e5d5d',
     alignItems: 'center',
     justifyContent: 'center',
   },
