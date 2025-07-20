c60a370160857d7fd6c80e747ba25ed3b1651cc6
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-07-20 16:00:52 -0300

    feat: create menu component.

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+import { styles } from './MenuStyles'
+
+export function Menu() {
+  return (
+    <View>
+      <Text style={styles.title}>Menu component</Text>
+    </View>
+  )
+}
